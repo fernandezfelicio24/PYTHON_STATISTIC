d76ff90167fe30af64de1d7d416ca6271e4ae0c1
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2021-09-22 21:56:17 +0700

    mode and so on

--- Statistic/statis1.py
+++ Statistic/statis1.py
@@ -1,16 +1,33 @@
+import math
 
 def mean(*args):
     val_sum = sum(args)
     return val_sum / len(args)
     
 
-
-
-
+def median(*args):
+    if len(args) % 2 == 0:
+        i = round((len(args) + 1 ) / 2)
+        j = i - 1
+        return (args[i] + args[j]) / 2
+    else:
+        k = round(len(args) / 2)
+        return args[k]
+    
+def mode(*args):
+    #count how many values show up in
+    #the list and put it in a dictionary
+    dict_vals = {i: args.count(i) for i in args}
+    #print(type(dict_vals))
+    #print("chek value nilai dictionary :" )
+    max_list = [k for k, v in dict_vals.items() if v == max(dict_vals.values())]
+    return max_list
 
 def main():
     print(f"Mean : {mean(1,2,3,4,5)}")
-
+    print(f"Median : {median(1,2,3,4,5)}")
+    print(f"Median : {median(1,2,3,4,5,6)}")
+    print(f"Mode : {mode(1, 2, 3, 4, 5, 4)}")
 
 
 
